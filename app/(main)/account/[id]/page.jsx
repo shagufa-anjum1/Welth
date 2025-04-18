72060922097f09dfb0703bc47339697cc56fa5e9
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2025-04-19 02:52:24 +0530

    Added Seed File

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -12,15 +12,23 @@ const AccountsPage = async ({ params }) => {
   const { transactions, ...account } = accountData;
 
   return (
-    <div>
+    <div className="space-y-8 px-5 flex gap-4 items-end justify-between">
       <div>
-        <h1>{account.name}</h1>
-        <p>{account.type.charAt(0).toUpperCase() + account.type.slice(1).toLowerCase()} Account</p>
+        <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">{account.name}</h1>
+        <p className="text-muted-foreground">
+        {account.type.charAt(0).toUpperCase() + account.type.slice(1).toLowerCase()} Account</p>
       </div>
-      <div>
-        <div>${parseFloat(account.balance).toFixed(2)}</div>
-        <p>{account._count.transactions} Transactions</p>
+
+
+      <div className="text-right pb-2">
+        <div className="text-xl sm:text-2xl font-bold">${parseFloat(account.balance).toFixed(2)}</div>
+        <p className="text-muted-foreground text-sm">{account._count.transactions} Transactions</p>
       </div>
+
+        {/* chart Section */}
+
+        {/* Transaction Table */}
+      
     </div>
   );
 };
