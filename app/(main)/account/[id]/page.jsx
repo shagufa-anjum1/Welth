5a3db225723770521f9047acebddd1fe0b24202d
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2025-04-23 17:28:00 +0530

    Transactions Rendering

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,13 +1,16 @@
-import { getAccountWithTransactions } from "@/actions/accounts";
-import { notFound } from "next/navigation";
-import React, { Suspense } from "react";
+import { Suspense } from "react";
+// import { getAccountWithTransactions } from "@/actions/account";
 import { BarLoader } from "react-spinners";
-import TransactionTable from "../_components/transaction-table";
+import { TransactionTable } from "../_components/transaction-table";
+import { notFound } from "next/navigation";
+import { AccountChart } from "../_components/account-chart";
+import { getAccountWithTransactions } from "@/actions/accounts";
 
-const AccountsPage = async ({ params }) => {
-  const { id } = await params; // ✅ Fix: await params before using it
+export default async function AccountPage({ params }) {
+  const { id } = await params; // ✅ await destructuring
 
   const accountData = await getAccountWithTransactions(id);
+
 
   if (!accountData) {
     notFound();
@@ -19,12 +22,11 @@ const AccountsPage = async ({ params }) => {
     <div className="space-y-8 px-5">
       <div className="flex gap-4 items-end justify-between">
         <div>
-          <h1 className="text-5xl sm:text-6xl font-bold gradient-title capitalize">
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
             {account.name}
           </h1>
           <p className="text-muted-foreground">
-            {account.type.charAt(0).toUpperCase() +
-              account.type.slice(1).toLowerCase()}{" "}
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
             Account
           </p>
         </div>
@@ -33,22 +35,25 @@ const AccountsPage = async ({ params }) => {
           <div className="text-xl sm:text-2xl font-bold">
             ${parseFloat(account.balance).toFixed(2)}
           </div>
-          <p className="text-muted-foreground text-sm">
+          <p className="text-sm text-muted-foreground">
             {account._count.transactions} Transactions
           </p>
         </div>
       </div>
 
       {/* Chart Section */}
-      {/* Transaction Table */}
       <Suspense
         fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
       >
-        {/* @ts-expect-error Server Component */}
+        <AccountChart transactions={transactions} />
+      </Suspense>
+
+      {/* Transactions Table */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
         <TransactionTable transactions={transactions} />
       </Suspense>
     </div>
   );
-};
-
-export default AccountsPage;
+}
